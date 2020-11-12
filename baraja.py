1cd98546d0e2cb3ad0bbab010c369445f913374c
angelluis21 <anyel_luis21@hotmail.com>
2020-11-12 22:54:29 +0100

    Programa de baraja terminado

--- baraja.py
+++ baraja.py
@@ -4,25 +4,31 @@ palos = ['o','c','e','b']
 numeros = ['A','2','3','4','5','6','7','S','C','R']
 
 
-def creaBaraja(palos, numeros):
+def crearBaraja(palos, numeros):
     baraja = []
     for palo in palos:
         for numero in numeros:
             baraja.append(numero+palo)
     return baraja     
 
-'''
+
 def intercambio(primer_valor, segundo_valor):
     aux = primer_valor
     primer_valor = segundo_valor
     segundo_valor = aux
     return primer_valor, segundo_valor
-'''
+
 
 def barajar(lista_de_naipes):
     for i in range(len(lista_de_naipes)):
         nuevas_pos = random.randrange(len(lista_de_naipes))
+
+        '''
+        intercambio de cartas, (tecnica de vaso vacio)
+        '''
         aux = lista_de_naipes[nuevas_pos]
         lista_de_naipes[nuevas_pos] = lista_de_naipes[i]
         lista_de_naipes[i] = aux
     return lista_de_naipes
+
+
--- barajaC.py
+++ barajaC.py
@@ -5,10 +5,22 @@ class Baraja():
     def __init__(self, palos, numeros):
         self.palos = palos
         self.numeros = numeros
-        self.mazacote = Baraja.creaBaraja(palos, numeros)
+        self.mazacote = baraja.crearBaraja(palos, numeros)
 
 
-def baraja(self):
-    baraja.baraja(self.mazacote)
+    def barajar(self):
+        baraja.barajar(self.mazacote)
     
+
+    def repartir(self, num_jugadores, num_cartas):
+        jugadores = []
+        for i in range(num_jugadores):
+            jugadores.append([])
+        
+
+        for jugador in range(num_jugadores):
+            for carta in range(num_cartas):
+                jugadores[jugador].append(self.mazacote.pop(0)) # en esta línea dice: añademe en jugadores[jugador] lo que quites de self.mazacote 
+        return jugadores
+
 
